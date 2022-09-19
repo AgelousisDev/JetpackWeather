47197789fc3022c611adee1909641f4aeff3f870
Evangelos Angelousis <vage.solutions@gmail.com>
2022-09-19 15:15:54 +0300

    Jetpack Weather
    Updates

--- app/src/main/java/com/agelousis/jetpackweather/appWidget/WeatherAppWidget.kt
+++ app/src/main/java/com/agelousis/jetpackweather/appWidget/WeatherAppWidget.kt
@@ -1,33 +1,27 @@
 package com.agelousis.jetpackweather.appWidget
 
-import android.graphics.BitmapFactory
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.Dimension
 import androidx.datastore.preferences.core.Preferences
 import androidx.glance.*
 import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.layout.Alignment
-import androidx.glance.layout.Column
-import androidx.glance.layout.padding
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.layout.*
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
-import coil.compose.rememberAsyncImagePainter
+import androidx.glance.unit.ColorProvider
+import com.agelousis.jetpackweather.R
 import com.agelousis.jetpackweather.appWidget.helper.CustomGlanceStateDefinition
 import com.agelousis.jetpackweather.mapAddressPicker.AddressDataModel
-import com.agelousis.jetpackweather.network.response.CurrentWeatherDataModel
-import com.agelousis.jetpackweather.utils.extensions.toModel
-import com.agelousis.jetpackweather.utils.extensions.urlBitmap
-import com.agelousis.jetpackweather.utils.extensions.valueEnumOrNull
+import com.agelousis.jetpackweather.network.response.WeatherResponseModel
+import com.agelousis.jetpackweather.utils.constants.Constants
+import com.agelousis.jetpackweather.utils.extensions.*
 import com.agelousis.jetpackweather.utils.helpers.PreferencesStoreHelper
 import com.agelousis.jetpackweather.weather.enumerations.TemperatureUnitType
-import java.net.URL
+import java.util.*
 
 class WeatherAppWidget: GlanceAppWidget() {
 
@@ -38,10 +32,10 @@ class WeatherAppWidget: GlanceAppWidget() {
     override fun Content() {
         val context = LocalContext.current
         val preferences = currentState<Preferences>()
-        val currentWeatherDataModel = remember {
+        val weatherResponseModel = remember {
             preferences[
                     PreferencesStoreHelper.WEATHER_RESPONSE_MODE_DATA_KEY
-            ]?.toModel<CurrentWeatherDataModel>()
+            ]?.toModel<WeatherResponseModel>()
         }
         val addressDataModel = remember {
             preferences[
@@ -55,27 +49,185 @@ class WeatherAppWidget: GlanceAppWidget() {
         }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalAlignment = Alignment.Vertical.CenterVertically
-        ) {
-            Text(
-                text = currentWeatherDataModel?.currentTemperatureUnitFormatted(
-                    temperatureUnitType = temperatureUnitType
-                ) ?: "",
-                style = TextStyle(
-                    fontSize = 24.sp
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = GlanceModifier
+                .fillMaxSize()
+                .background(
+                    colorProvider = ColorProvider(
+                        R.color.whiteTwo
+                    )
                 )
-            )
-            Image(
-                provider = ImageProvider(
-                    bitmap = currentWeatherDataModel?.weatherConditionDataModel?.iconUrl?.urlBitmap
-                        ?: return@Column
+                .cornerRadius(
+                    radius = 32.dp
+                )
+        ) {
+            // Location & Time
+            Text(
+                text = "${addressDataModel?.addressLine}\n${Date().toDisplayDate(
+                    pattern = Constants.DISPLAY_DATE_TIME_FORMAT
+                )}",
+                style = TextStyle(
+                    fontSize = 14.sp
                 ),
-                contentDescription = null,
+                modifier = GlanceModifier
+                    .padding(
+                        top = 16.dp
+                    )
+            )
+            Row(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = GlanceModifier
+                    .padding(
+                        top = 16.dp
+                    )
+            ) {
+                // C
+                Text(
+                    text = weatherResponseModel?.currentWeatherDataModel?.currentTemperatureUnitFormatted(
+                        temperatureUnitType = temperatureUnitType
+                    ) ?: "",
+                    style = TextStyle(
+                        fontSize = 24.sp
+                    )
+                )
+                val iconBitmap = weatherResponseModel?.currentWeatherDataModel?.weatherConditionDataModel?.iconUrl?.urlBitmap
+                if (iconBitmap != null)
+                    // Condition Icon
+                    Image(
+                        provider = ImageProvider(
+                            bitmap = iconBitmap
+                        ),
+                        contentDescription = null,
+                        modifier = GlanceModifier
+                            .padding(
+                                start = 16.dp
+                            )
+                            .size(
+                                size = 40.dp
+                            )
+                    )
+            }
+            // Condition Text
+            Text(
+                text = weatherResponseModel?.currentWeatherDataModel?.weatherConditionDataModel?.text
+                    ?: "",
+                style = TextStyle(
+                    color = ColorProvider(R.color.grey),
+                    fontSize = 14.sp
+                ),
                 modifier = GlanceModifier
                     .padding(
                         top = 8.dp
                     )
             )
+            // Feels Like
+            Text(
+                text = context.resources.getString(
+                    R.string.key_feels_like_label,
+                    weatherResponseModel?.currentWeatherDataModel?.feelsLikeTemperatureUnitFormatted(
+                        temperatureUnitType = temperatureUnitType
+                    ) ?: ""
+                ),
+                style = TextStyle(
+                    color = ColorProvider(R.color.grey),
+                    fontSize = 14.sp
+                ),
+                modifier = GlanceModifier
+                    .padding(
+                        top = 8.dp
+                    )
+            )
+            Spacer(
+                modifier = GlanceModifier
+                    .padding(
+                        top = 8.dp
+                    )
+            )
+            // Wind Layout
+            Row(
+                modifier = GlanceModifier
+                    .padding(
+                        top = 16.dp
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = weatherResponseModel?.currentWeatherDataModel?.windKph?.toInt()?.toString()
+                        ?: "",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        color = ColorProvider(
+                            resId = weatherResponseModel?.currentWeatherDataModel?.windStateColor
+                                ?: R.color.colorAccent
+                        )
+                    )
+                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = GlanceModifier
+                        .padding(
+                            start = 16.dp
+                        )
+                ) {
+                    if (weatherResponseModel?.currentWeatherDataModel?.windDegree != null)
+                        Image(
+                            provider = ImageProvider(
+                                resId = R.drawable.ic_arrow_direction_down
+                            ),
+                            contentDescription = null,
+                            modifier = GlanceModifier
+                                .size(
+                                    size = 15.dp
+                                )
+                        )
+                    Text(
+                        text = context.resources.getString(R.string.key_km_hourly_label),
+                        style = TextStyle(
+                            color = ColorProvider(R.color.grey),
+                            fontSize = 12.sp,
+                        ),
+                        modifier = GlanceModifier
+                            .padding(
+                                top = 8.dp
+                            )
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = GlanceModifier
+                        .padding(
+                            start = 16.dp
+                        )
+                ) {
+                    Text(
+                        text = weatherResponseModel?.currentWeatherDataModel?.getWindStateWarning(
+                            context = context
+                        ) ?: "",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            color = ColorProvider(
+                                resId = weatherResponseModel?.currentWeatherDataModel?.windStateColor
+                                    ?: R.color.colorAccent
+                            )
+                        )
+                    )
+                    Text(
+                        text = context.resources.getString(
+                            R.string.key_now_with_value_label,
+                            weatherResponseModel?.currentWeatherDataModel?.getWindDirection(
+                                context = context
+                            ) ?: ""
+                        ),
+                        style = TextStyle(
+                            fontSize = 14.sp
+                        )
+                    )
+                }
+            }
         }
     }
 
