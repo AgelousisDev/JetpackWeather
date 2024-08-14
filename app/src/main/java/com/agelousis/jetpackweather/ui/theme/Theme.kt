59fed86dc86e52fd05e09206aa59ae26a0d34b84
Evangelos Angelousis <vagelis_agelousis@outlook.com>
2024-08-14 14:52:49 +0300

    Dependency Updates

--- app/src/main/java/com/agelousis/jetpackweather/ui/theme/Theme.kt
+++ app/src/main/java/com/agelousis/jetpackweather/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package com.agelousis.jetpackweather.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -10,8 +12,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -55,8 +55,9 @@ fun JetpackWeatherTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window?.navigationBarColor = Color.Transparent.toArgb()
-            (view.context as Activity).window?.statusBarColor = colorScheme.surface.toArgb()
+            (view.context as? ComponentActivity)?.enableEdgeToEdge()
+            //(view.context as Activity).window?.navigationBarColor = Color.Transparent.toArgb()
+            //(view.context as Activity).window?.statusBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController((view.context as Activity).window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
